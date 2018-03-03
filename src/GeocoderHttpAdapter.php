b36ca310df2bff6561e1353dbd13ff3d1120665b
itamair <itamair@me.com>
2018-03-03 17:25:11 +0100

    change_http_adapter-2796277-13.patch applied

---
+++ src/GeocoderHttpAdapter.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\geocoder;
+
+use GuzzleHttp\ClientInterface;
+use Ivory\HttpAdapter\AbstractHttpAdapter;
+use Ivory\HttpAdapter\Message\InternalRequestInterface;
+/**
+ * Extends AbstractHttpAdapter to provide Guzzle.
+ */
+class GeocoderHttpAdapter extends AbstractHttpAdapter {
+
+  /**
+   * The HTTP client to fetch the feed data with.
+   *
+   * @var \GuzzleHttp\Client
+   */
+  protected $httpClient;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return 'geocoder_http_adapter';
+  }
+
+  /**
+   * Creates an http adapter.
+   *
+   * @param \GuzzleHttp\ClientInterface $httpClient
+   *   A Guzzle client object.
+   */
+  public function __construct(ClientInterface $httpClient) {
+    parent::__construct();
+    $this->httpClient = $httpClient;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function sendInternalRequest(InternalRequestInterface $internalRequest) {
+    $response = $this->httpClient->request($internalRequest->getMethod(), (string) $internalRequest->getUri(), [
+      'headers' => $this->prepareHeaders($internalRequest, FALSE, FALSE),
+    ]);
+    return $response;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function sendInternalRequests(array $internalRequests, $success, $error) {
+    foreach ($internalRequests as $internalRequest) {
+      $response = $this->httpClient->request($internalRequest->getMethod(), (string) $internalRequest->getUri(), [
+        'headers' => $this->prepareHeaders($internalRequest, FALSE, FALSE),
+      ]);
+      call_user_func($success, $response);
+    }
+  }
+
+}
