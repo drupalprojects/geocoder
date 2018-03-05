33413d9a8a42a056e0379960d97aabc2203d3bc3
itamair <itamair@me.com>
2018-03-05 09:41:23 +0100

    Added geocoder.settings.plugins schema for plugins without arguments, for general schema configuration validation, and related check in the SettingsForm

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -167,7 +167,7 @@ class SettingsForm extends ConfigFormBase {
 
       if ($this->typedConfigManager->hasConfigSchema('geocoder.settings.plugins.' . $plugin['id'])) {
         $plugin_config_schema = $this->typedConfigManager->getDefinition('geocoder.settings.plugins.' . $plugin['id']);
-        $plugin_config_schema = $plugin_config_schema['mapping'];
+        $plugin_config_schema = isset($plugin_config_schema['mapping']) ? $plugin_config_schema['mapping'] : [];
       }
 
       $rows[$plugin['id']] = [
